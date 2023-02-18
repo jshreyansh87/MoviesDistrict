7c35241d4ea3dae49967d7d33d02dfdc3f51059b
Shreyansh Jain <jshreyansh87@gmail.com>
2023-02-18 20:19:17 +0530

    Setup index file. Setup for axios and tmdb api

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import http from "http";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+const port = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("MongoDB connnected");
+    server.listen(port, () => {
+        console.log(`Server Listening at http://localhost:${port}`)
+    });
+}).catch((err) => {
+    console.log({ err });
+    process.exit(1);
+});
---
+++ server/src/tmdb/tmdb.api.js
@@ -0,0 +1,8 @@
+import axiosClient from '../axios/axios.client.js';
+import tmdbEndpoints from '../tmdb/tmdb.endpoints.js';
+
+const tmdbApi = {
+
+};
+
+export default tmdbApi;
