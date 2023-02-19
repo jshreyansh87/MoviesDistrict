4efd36909656fbe9d32de764abc69e0ed2e8cc92
Shreyansh Jain <jshreyansh87@gmail.com>
2023-02-19 18:47:27 +0530

    Add tmdb api calls

--- server/src/tmdb/tmdb.api.js
+++ server/src/tmdb/tmdb.api.js
@@ -2,7 +2,36 @@ import axiosClient from '../axios/axios.client.js';
 import tmdbEndpoints from '../tmdb/tmdb.endpoints.js';
 
 const tmdbApi = {
-
+    mediaList: async ({ mediaType, mediaCategory, page }) => await axiosClient.get(
+        tmdbEndpoints.mediaList({ mediaType, mediaCategory, page })
+    ),
+    mediaDetails: async ({ mediaType, mediaId }) => await axiosClient.get(
+        tmdbEndpoints.mediaDetails({ mediaType, mediaId })
+    ),
+    mediaGenres: async ({ mediaType }) => await axiosClient.get(
+        tmdbEndpoints.mediaGenres({ mediaType })
+    ),
+    mediaCredits: async ({ mediaType, mediaId }) => await axiosClient.get(
+        tmdbEndpoints.mediaCredits({ mediaType, mediaId })
+    ),
+    mediaVideos: async ({ mediaType, mediaId }) => await axiosClient.get(
+        tmdbEndpoints.mediaVideos({ mediaType, mediaId })
+    ),
+    mediaRecommend: async ({ mediaType, mediaId }) => await axiosClient.get(
+        tmdbEndpoints.mediaRecommend({ mediaType, mediaId })
+    ),
+    mediaImages: async ({ mediaType, mediaId }) => await axiosClient.get(
+        tmdbEndpoints.mediaImages({ mediaType, mediaId })
+    ),
+    mediaSearch: async ({ mediaType, query, page }) => await axiosClient.get(
+        tmdbEndpoints.mediaSearch({ mediaType, query, page })
+    ),
+    personDetail: async ({ personId }) => await axiosClient.get(
+        tmdbEndpoints.personDetail({ personId })
+    ),
+    personMedias: async ({ personId }) => await axiosClient.get(
+        tmdbEndpoints.personMedias({ personId })
+    ),
 };
 
 export default tmdbApi;
