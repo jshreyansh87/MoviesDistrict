d4d5274163171094f7f0288d76ddc8349f24d6e8
Shreyansh Jain <jshreyansh87@gmail.com>
2023-02-22 00:01:41 +0530

    Add media getDetails controller method

--- server/src/controllers/media.controller.js
+++ server/src/controllers/media.controller.js
@@ -3,6 +3,8 @@ import tmdbApi from '../tmdb/tmdb.api.js';
 import userModel from '../models/user.model.js';
 import favoriteModel from '../models/favorite.model.js';
 import reviewModel from '../models/review.model.js';
+import tokenMiddleware from '../middlewares/token.middleware.js';
+import { model } from 'mongoose';
 
 const getList = async (req, res) => {
     try {
@@ -44,4 +46,49 @@ const search = async (req, res) => {
     } catch {
         responseHandler.error(res);
     }
+};
+
+const getDetails = async (req, res) => {
+    try {
+        const { mediaType, mediaId } = req.params;
+        const params = { mediaType, mediaId };
+
+        const media = await tmdbApi.mediaDetails(params);
+
+        media.credits = await tmdbApi.mediaCredits(params);
+
+        const videos = await tmdbApi.mediaVideos(params);
+
+        media.videos = videos;
+
+        const recommend = await tmdbApi.mediaRecommend(params);
+
+        media.recommend = recommend.results;
+
+        media.images = await tmdbApi.mediaImages(params);
+
+        const tokenDecoded = tokenMiddleware.tokenDecode(req);
+
+        if (tokenDecoded) {
+            const user = await userModel.findById(tokenDecoded.data);
+
+            if (user) {
+                const isFavorite = await favoriteModel.findOne({ user: user.id, mediaId });
+                model.isFavorite = isFavorite !== null;
+            }
+        }
+
+        media.reviews = await reviewModel.find({mediaId}).populate("user").sort("-createdAt");
+
+        return responseHandler.ok(res, response);
+    } catch {
+        responseHandler.error(res);
+    }
+};
+
+export default {
+    getList,
+    getGenres,
+    search,
+    getDetails
 };
