c91ff3c61ea0b218246228e002d413bc735062fd
Shreyansh Jain <jshreyansh87@gmail.com>
2023-02-21 23:41:55 +0530

    Add media getList controller method

---
+++ server/src/controllers/media.controller.js
@@ -0,0 +1,18 @@
+import responseHandler from '../handlers/response.handler.js';
+import tmdbApi from '../tmdb/tmdb.api.js';
+import userModel from '../models/user.model.js';
+import favoriteModel from '../models/favorite.model.js';
+import reviewModel from '../models/review.model.js';
+
+const getList = async (req, res) => {
+    try {
+        const { page } = req.query;
+        const { mediaType, mediaCategory } = req.params;
+
+        const response = await tmdbApi.mediaList({ mediaType, mediaCategory, page });
+
+        return responseHandler.ok(res, response);
+    } catch {
+        responseHandler.error(res);
+    }
+};
