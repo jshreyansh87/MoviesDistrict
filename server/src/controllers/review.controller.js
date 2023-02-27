9e8fce883d36c6cf13dafa5f5c1bc08c2dd0b3e6
Shreyansh Jain <jshreyansh87@gmail.com>
2023-02-27 23:42:43 +0530

    Add review getReviewOfUser controller method

--- server/src/controllers/review.controller.js
+++ server/src/controllers/review.controller.js
@@ -41,3 +41,21 @@ const remove = async (req, res) => {
         responseHandler.error(res);
     }
 };
+
+const getReviewOfUser = async (req, res) => {
+    try {
+        const reviews = await reviewModel.find({
+            user: req.user.id
+        }).sort("-createdAt");
+
+        responseHandler.ok(res, reviews);
+    } catch {
+        responseHandler.error(res);
+    }
+};
+
+export default {
+    create,
+    remove,
+    getReviewOfUser
+};
