4821e6a44ee105a2426288dd862f88d832a8fb4b
Shreyansh Jain <jshreyansh87@gmail.com>
2023-03-03 23:32:39 +0530

    Update review api client service. Updated api method for delete api

--- client/src/api/modules/review.api.js
+++ client/src/api/modules/review.api.js
@@ -33,7 +33,7 @@ const reviewApi = {
     },
     remove: async ({ reviewId }) => {
         try {
-            const response = await privateClient.post(reviewEndpoints.remove({ reviewId }));
+            const response = await privateClient.delete(reviewEndpoints.remove({ reviewId }));
 
             return { response };
         } catch (err) {
