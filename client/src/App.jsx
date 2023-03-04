3e3d330872a9cab5a5b94f6bf696208c65750afe
Shreyansh Jain <jshreyansh87@gmail.com>
2023-03-04 14:55:11 +0530

    Configure theme in app.js, configure toastify and mui css reset

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
+import { ThemeProvider } from "@mui/material/styles";
+import { useSelector } from 'react-redux';
+import themeConfigs from "./configs/theme.configs";
+import { ToastContainer } from 'react-toastify'
+import CssBaseline from "@mui/material/CssBaseline";
+
 const App = () => {
+  const { themeMode } = useSelector((state) => state.themeMode);
+
   return (
-    <div className="App">
-      Movies District
-    </div>
+    <ThemeProvider theme={themeConfigs.custom({ mode: themeMode })}>
+      {/* setup toastify */}
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+        theme={themeMode}
+      />
+      {/* mui css reset */}
+      <CssBaseline />
+    </ThemeProvider>
   );
 }
 
